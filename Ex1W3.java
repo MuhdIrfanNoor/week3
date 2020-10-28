d03cfec229731019b2683b3aabeab38e1a22317b
MuhdIrfanNoor <muhdirfan272001@gmail.com>
2020-10-28 19:40:10 +0800

    week3

---
+++ Ex1W3.java
@@ -0,0 +1,23 @@
+//nama : muhammad irfan bin mahamad noor
+//group: S3G2
+
+public class Ex1W3{
+	public static void main(String [] hello){
+
+
+		System.out.println("nilai 1:" + hello[0]);
+		System.out.println("nilai 1:" + hello[1]);
+		System.out.println("nilai 1:" + hello[2]);
+
+		int sum =0;
+		for(int i=0;i<hello.length;i++){
+			System.out.println("nilai 1:" + hello[i]);
+			sum+=Integer.parseInt(hello[i]);
+			System.out.println("nilai 1:" + sum);
+		}
+
+
+	}
+	//java ExWeek3 10 20 30 40
+
+}
---
+++ Exweek3.java
@@ -0,0 +1,80 @@
+//nama : muhammad irfan bin mahamad noor
+//group: S3G2
+
+import java.util.Scanner;
+
+public class Exweek3{
+
+	public static void main(String[] args) {
+
+
+
+		// String nama;
+		// int nombor;
+		// double noDouble;
+
+		// Scanner input= new Scanner(System.in);
+		
+		// System.out.println("Enter a number: ");
+		// nombor = input.nextInt();
+		// System.out.println("number is " + nombor);
+
+		// input.nextLine();
+
+		// System.out.println("Enter a name: ");
+		// nama = input.nextLine(); // input.nextLine();
+		// System.out.println("name is " + nama);
+
+		// //input.nextLine();
+
+		// System.out.println("Enter a double: ");
+		// noDouble = input.nextDouble();
+		// System.out.println("number is " + noDouble);
+
+		// input.close();
+
+		// int [] tataInt ={10,11,12,13};
+		// for (int i=0; i<tataInt.length;i++){
+		// 	System.out.println(tataInt[i]);
+		// }
+
+		// for( int t:tataInt ){
+		// 	System.out.println("print each : " + t );
+		// }
+
+
+		int noInt=10,x;
+
+		// if (noInt == 10)
+		// 	System.out.println("sama");
+		// else
+		// 	System.out.println("X sama");
+		// noInt=2;
+		//tenery
+		System.out.println(noInt==10 ? "sama" :"X sama" ); 
+
+		
+
+		x = (noInt == 10 ? 3:19 );
+		System.out.println("x is "+ x );
+
+		//final = constants
+
+		//final static char CHARS=53;
+		//System.out.format("char " + CHARS);
+		//CHARS=100;
+		//System.out.format("char " + CHARS);
+
+		//int noInt2= 343;
+		//double noD=123.907;
+		//System.out.format("%6d %4d",noInt,noInt2);
+		//System.out.format(" %4d",noInt2);
+		//System.out.format("RM%7.1f",noD);
+		//System.out.printf("\nRM%7.3f dan %4d ",noD,noInt);
+	}
+
+
+	//% = untuk titik perpuluhan cth %7.3f akan sediakan 7 tempat, 1 utk berape bnyk perpuluhan
+
+
+}
